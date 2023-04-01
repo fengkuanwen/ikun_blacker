6f924c91403a85425641b6008a1942b08e8dd40d
76177 <761774318@qq.com>
2023-04-01 18:47:15 +0800

    "授权模块初次提交"

--- ym_fore/src/views/About.vue
+++ ym_fore/src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button class="btn btn-primary">这是一个啥子</button>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'about',
+  components: {
+  }
+}
+</script>
