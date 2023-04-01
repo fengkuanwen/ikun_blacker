a9721efb8ae9050d8057f767a9a028e9456828e9
76177 <761774318@qq.com>
2023-04-02 03:24:36 +0800

    "前端生成验证码"

--- ym_fore/src/components/login/PwdLogin.vue
+++ ym_fore/src/components/login/PwdLogin.vue
@@ -6,8 +6,13 @@
                 <input type="text" placeholder="手机号" id="username" maxlength="11" required>
             </div>
             <div class="item">
-                <input type="text" placeholder="邮箱" id="password" required>
+                <input type="password" placeholder="密码" id="password" required>
             </div>
+            <div class="item">
+                <input type="text" placeholder="验证码" id="captcha" required style="width:90px">
+                <img :src="captchaSrc" @click="changeCaptcha" alt="验证码" width="90px" height="45px;" style="padding-bottom: 5px"/>
+            </div>
+            
             <div>
                 <button id="btn">登陆</button>
             </div>
@@ -18,7 +23,26 @@
 <script>
   export default {
     name: 'pwdLogin',
+    data(){
+        return{
+            captchaSrc : ""
+        }
+    },
     components: {
-    }
+    },
+    methods:{
+        changeCaptcha(){
+            var timeStamp = new Date().getTime(); 
+            this.$store.commit("changeCaptchaTimeStampCache",timeStamp)
+            this.captchaSrc = this.$store.state.serverPath+"/auth/captcha/login?timeStamp="+timeStamp;
+        },
+        initMethod(){
+            // 进入界面加载验证码
+            this.changeCaptcha();
+        },
+    },
+    mounted() {
+        this.initMethod();
+    },
   }
 </script>
