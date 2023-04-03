23ef4393606e6659e41e80220ae23c2e8f771965
fengkuanwen <761774318@qq.com>
2023-04-03 16:40:59 +0800

    修改目录结构

--- ym_shopping/oauth-server/src/main/java/org/example/controller/CaptchaController.java
+++ ym_shopping/oauth-server/src/main/java/org/example/controller/CaptchaController.java
@@ -37,14 +37,12 @@ public class CaptchaController {
     public void getRegistryCaptcha(@RequestParam("timeStamp") String timeStamp, HttpServletResponse response) throws Exception{
         //定义图形验证码的长和宽
         LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(200, 100);
+//        获取手机短信的 验证码字符直接传输到前端，让前端自我验证
         Cookie cookie = new Cookie("registryCaptchaCode",lineCaptcha.getCode());
         cookie.setPath("/");
         response.addCookie(cookie);
         ServletOutputStream outputStream = response.getOutputStream();
-//    ("registryCaptchaCode",lineCaptcha.getCode());
-        //图形验证码写出到流
         lineCaptcha.write(outputStream);
-        //redis生成全局ID 存储验证码 ：前端验证时需携带timeStamp时间戳
         outputStream.flush();
         outputStream.close();
     }
